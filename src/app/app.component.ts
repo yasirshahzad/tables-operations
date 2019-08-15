ef4f5dfd485ef9b7e73401b27056a19b2a33ce10
Muhammad Yasir <admin@yasir.com.pk>
2019-08-15 15:45:20 +0500

    Table Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tables';
+  listOfData: any[] = [];
+  bordered = false;
+  loading = false;
+  sizeChanger = false;
+  pagination = true;
+  header = true;
+  title = true;
+  footer = true;
+  fixHeader = false;
+  size = 'small';
+  expandable = true;
+  checkbox = true;
+  allChecked = false;
+  indeterminate = false;
+  displayData: any[] = [];
+  simple = false;
+  noResult = false;
+  position = 'bottom';
+
+  currentPageDataChange(
+    $event: Array<{
+      name: string;
+      age: number;
+      address: string;
+      checked: boolean;
+      expand: boolean;
+      description: string;
+    }>
+  ): void {
+    this.displayData = $event;
+    this.refreshStatus();
+  }
+
+  refreshStatus(): void {
+    const validData = this.displayData.filter(value => !value.disabled);
+    const allChecked = validData.length > 0 && validData.every(value => value.checked === true);
+    const allUnChecked = validData.every(value => !value.checked);
+    this.allChecked = allChecked;
+    this.indeterminate = !allChecked && !allUnChecked;
+  }
+
+  checkAll(value: boolean): void {
+    this.displayData.forEach(data => {
+      if (!data.disabled) {
+        data.checked = value;
+      }
+    });
+    this.refreshStatus();
+  }
+
+  ngOnInit(): void {
+    for (let i = 1; i <= 100; i++) {
+      this.listOfData.push({
+        name: 'John Brown',
+        age: `${i}2`,
+        address: `New York No. ${i} Lake Park`,
+        description: `My name is John Brown, I am ${i}2 years old, living in New York No. ${i} Lake Park.`,
+        checked: false,
+        expand: false
+      });
+    }
+  }
+
+  noResultChange(status: boolean): void {
+    this.listOfData = [];
+    if (!status) {
+      this.ngOnInit();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+//import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { AppComponent } from './app.component';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NzTableModule,
+    NzFormModule,
+    NzSwitchModule,
+    NzRadioModule,
+    NzDividerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
